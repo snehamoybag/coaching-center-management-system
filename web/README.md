9224cb43abdc70a2b37040315f8eab771956c581
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2026-01-13 10:08:25 +0530

    chore(web): setup react router + tailwindcss

---
+++ web/README.md
@@ -0,0 +1,1 @@
+# Coaching Center Admin Dashboard
---
+++ web/src/routes.tsx
@@ -0,0 +1,11 @@
+import type { RouteObject } from "react-router";
+import Root from "./Root";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Root />,
+  },
+];
+
+export default routes;
